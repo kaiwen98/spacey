aa821ce7d3862a4537155ee5e3d81fc698abba0b
kaiwen98 <e0406836@u.nus.edu>
2020-07-27 03:11:48 +0800

    lol

--- Server/Telegram_Bot_Spacey/bot.py
+++ Server/Telegram_Bot_Spacey/bot.py
@@ -57,14 +57,14 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    """
+    
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://spaceyherok.herokuapp.com/' + TOKEN)
     """
     updater.start_polling()
-    
+    """
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 root = dirname(abspath(__file__))
 sys.path.append(join(root, "Server"))
 sys.path.append(join(root, "Server", "Telegram_Bot_Spacey"))
-from Server.Telegram_Bot_Spacey import test
+from Server.Telegram_Bot_Spacey import bot
 
 if __name__ == "__main__":
-    test.main()
+    bot.main()
