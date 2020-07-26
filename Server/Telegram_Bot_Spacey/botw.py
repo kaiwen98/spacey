ac2ea25e1024a8235b007afeca35f9a645199d92
kaiwen98 <e0406836@u.nus.edu>
2020-07-27 04:54:40 +0800

    lol

--- Server/Telegram_Bot_Spacey/botw.py
+++ Server/Telegram_Bot_Spacey/botw.py
@@ -8,7 +8,7 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 from os.path import dirname, abspath, join
-import redis
+#
 PORT = int(os.environ.get('PORT', 5000))
 
 _root = dirname(dirname(abspath(__file__)))
