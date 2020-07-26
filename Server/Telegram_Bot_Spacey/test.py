5edb4ca3c297d6b52fb011ba506d52cba766d5c0
kaiwen98 <e0406836@u.nus.edu>
2020-07-27 03:07:51 +0800

    lol

--- Server/Telegram_Bot_Spacey/test.py
+++ Server/Telegram_Bot_Spacey/test.py
@@ -1,90 +1,77 @@
 """
-import multiprocessing
-import time
-
-def calc_square(numbers, q):
-    while(True):
-        for n in numbers:
-            q.put(n*n)
-            time.sleep(0.2)
-
-    q.put(-1)
-    print('Exiting function')
-
-if __name__ == '__main__':
-    print('Now in the main code. Process name is:', __name__)
-    numbers = [2, 3, 4, 5]
-    q = multiprocessing.Queue()
-    p = multiprocessing.Process(target=calc_square, args=(numbers, q))
-    p.start()
-
-    while True:
-        nq = q.get()
-        print('Message is:', nq)
-        if nq == -1:
-            break
-
-    print('Done')
-    p.join()
-"""
-"""
-import time
-from multiprocessing import Process
- 
-class TestClass():
-    def test_f(self):
-        ctr = 0
-        while True:
-            ctr += 1
-            print("     ", ctr)
-            time.sleep(1.0)
- 
-if __name__ == '__main__':
-    ## run function in the background
-    CT=TestClass()
-    p = Process(target=CT.test_f)
-    p.start()
-
-    ## will not exit if function finishes, only when
-    ## "q" is entered, but this is just a simple example
-    stop_char=""
-    while stop_char.lower() != "q":
-        stop_char=input("Enter 'q' to quit ")
-        if stop_char.lower() == "u":
-            print("lol")
-    print("terminate process")
-    if p.is_alive():
-        p.terminate()
-
-
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Deployed using heroku.
+Author: liuhh02 https://medium.com/@liuhh02
 """
 
-from multiprocessing import Pool
-import time
-def myfunction(m):
-    while(true):
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
-        time.sleep(5)
+_root = dir(dir(abspath(__file__)))
 
-        
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+TOKEN = '1299596539:AAGD6nFaVfn2FF0EbhdXvLYlrGxSE-NQKr8'
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hip!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(_root)
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    """
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://spaceyherok.herokuapp.com/' + TOKEN)
+    """
+    updater.start_polling()
+    
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+def main2():
+    print("lol")
+
 if __name__ == '__main__':
-    my_pool = Pool(processes=4) # start 4 worker processes
-    result = my_pool.apply_async(myfunction, (10,)) # evaluate "f(10)" asynchronously in a single process
-    print (result.get(timeout=1))
-
-    print (my_pool.map(myfunction, range(10))) # prints "[0, 1, 4,..., 81]"
-    my_it = my_pool.imap(myfunction, range(10))
-    print (my_it.next() ) # prints "0"
-    print (my_it.next() ) # prints "1"
-    print (my_it.next(timeout=1) ) # prints "4" unless your computer is *very* slow
-    result = my_pool.apply_async(time.sleep, (10,))
-    print (result.get(timeout=1) ) # raises multiprocessing.TimeoutError
-    q = ""
-    while q != 'q':
-       
-        q = input("Press x to randomize")
-        if q  == 'x':
-
-                   
-        elif q == 'q':
-
+    main()
